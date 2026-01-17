67b0df065631e10fa9756a2e22cb04794db1ff95
Logicrithm <aviorsi08@gmail.com>
2026-01-17 15:27:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -877,4 +877,9 @@ if __name__ == '__main__':
     print("   Supports multilingual content")
     print("=" * 70)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    
+    
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
